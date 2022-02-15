a32653507b3e6bfd2cb399d7e932f4bca5c5aa53
Alessandra Chardon <achardon@ucla.edu>
2022-02-15 15:55:17 -0500

    changed drake_made_this method

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,20 +10,26 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
-    drake = ''
+
+    #this is the slower way of doing this
+    # drake = ''
     
-    Artist.all.each do |artist|
-      if artist.name == 'Drake'
-        drake = artist
-      else
-        false
-      end
-    end
+    # Artist.all.each do |artist|
+    #   if artist.name == 'Drake'
+    #     drake = artist
+    #   else
+    #     false
+    #   end
+    # end
     
-    if drake == ''
-      drake = Artist.create(name: 'Drake')
-    end
+    # if drake == ''
+    #   drake = Artist.create(name: 'Drake')
+    # end
+
+    #this method below is a faster way of doing this:
+    drake = Artist.find_or_create_by(name: 'Drake')
 
     self.artist = drake
+
   end
 end
